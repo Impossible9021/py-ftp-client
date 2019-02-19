6af45f2b5e784c92d5acddf1e7159453e8b41a6a
Impossible9021 <41771960+Impossible9021@users.noreply.github.com>
2019-02-19 19:11:31 +1100

    Little bugfixes & user/pass login support

--- main.py
+++ main.py
@@ -10,7 +10,16 @@ def connect():
         exit()
     
     port = input("Port number(def for default): ")
-    shell(host, port)
+    if (port != ""):
+        port = int(port) #convert port to int without getting ValueError when it's empty
+
+    user = input("User? (leave empty for anonymous login): ")
+    if (user == ""):
+        shell(host, port, "", "")
+    else:
+        passwd = str(input("Password: "))
+        shell(host, port, user, passwd)
+    
 
 def parse(input_str):
     """ A parser of user input for shell(). Returns command and args """
@@ -36,22 +45,29 @@ def parse(input_str):
         arg = ""
     return cmd, arg
 
-def shell(host = "", port = ""):
+def shell(host, port, user, passwd):
     """ Shell Mode function. Provides a CLI to interact with FTP server """
     #preparation: connect to the server
     try:
-        if (port == "def" or port == "" or port == None):
-            ftp = FTP(host)
-        else:
-            ftp = FTP(host, port)
-        ftp.login() # anon login only in this version
-        os.system("echo Entering Shell Mode in 3 seconds")
-        os.system("sleep 3")
+        if (user != ""):
+            if (port == "def" or port == "" or port == None):
+                ftp = FTP(host=host, user=user, passwd=passwd)
+        else: #anon login
+            if (port == "def" or port == "" or port == None):
+                ftp = FTP(host=host)
+                ftp.login()
+            else:
+                ftp = FTP(host=host, acct=port, user=user, passwd=str(passwd))
+                ftp.login()
+        print("Connecting to host '{0}' through port '{1}'... ".format(host, port))
+        
+        os.system("echo Entering Shell Mode in 2 seconds")
+        os.system("sleep 2")
         os.system("clear")
     except ftplib.all_errors as ex:
         print("An error occured while login: {0}".format(ex))
         return
-    print("Server response: " + ftp.getwelcome())
+    print("Server response:\n" + ftp.getwelcome())
     
     #enter shell mode
     while(1):
@@ -66,20 +82,23 @@ def shell(host = "", port = ""):
             ftp.quit()
             return
         if (cmd == "ls"):
-            print(ftp.retrlines('LIST'))
+            print(ftp.dir())
         if (cmd == "unknown"):
             print("Unknown command.")
 
 def print_help():
     print("This is a little FTP client written in Python 3\n")
     print("---Connect---")
-    print("Enter FTP server address and port.\n If you are going to use default port type 'def'")
+    print("Enter FTP server address and port.\n If you are going to use default port type 'def'\n\n")
     print("---Shell mode---")
     print("Usage is simular to bash/sh/zsh/whatever shell you are using.")
     print("Commands:")
     print("cd * - [C]hange (working) [D]irectory")
     print("upf * - [Up]load a [F]ile to the server")
-    print("exit - Quit the FTP connection")
+    print("exit - Quit the FTP connection\n\n")
+
+
+
 
 print("Welcome to PFTP - a little Python FTP Client for Linux")
 
